20b05e814935752b4d03005db53dfa065c671bd4
Mostafa Fouad <mostafafouad@Mostafas-MacBook-Pro.local>
2022-12-26 23:16:18 +0200

    Fix a few RTL bugs with footer links

--- components/SocialLink.tsx
+++ components/SocialLink.tsx
@@ -48,10 +48,12 @@ const Root = styled('div', {
         display: block;
         width: ${toRem(44)};
         height: ${toRem(44)};
+        margin-top: ${toRem(-0.5 * 44)};
+        margin-inline-start: ${toRem(-0.5 * 44)};
         pointer-events: none;
         opacity: 0;
         background: #fff;
-        transform: translate(-50%, -50%) scale(0.2);
+        transform: scale(0.2);
         border-radius: 50%;
         transition:
           200ms color,
@@ -64,7 +66,7 @@ const Root = styled('div', {
         &:before {
           opacity: 1;
           background: transparent;
-          transform: translate(-50%, -50%) scale(1);
+          transform: scale(1);
           transition:
             200ms color,
             200ms opacity,
@@ -82,28 +84,27 @@ const Root = styled('div', {
         display: block;
         width: ${toRem(44)};
         height: ${toRem(44)};
+        margin-top: ${toRem(-0.5 * 44)};
+        margin-inline-start: ${toRem(-0.5 * 44)};
         pointer-events: none;
         opacity: 0;
         background: #fff;
-        transform: translate(-50%, -50%);
+        transform: scale(1);
         border-radius: 50%;
         transition:
           100ms opacity linear 500ms,
-          10ms width ${easing.snapIn} 600ms,
-          10ms height ${easing.snapIn} 600ms,
-          300ms transform ${easing.snapIn} 300ms;
+          300ms margin-top ${easing.snapIn} 300ms,
+          0ms transform ${easing.snapIn} 600ms;
       }
 
       ${animationMode === 'in' && css`
         &:after {
-          width: ${toRem(20)};
-          height: ${toRem(20)};
           opacity: 1;
-          transform: translate(-50%, calc(-50% - ${toRem(45)}));
+          margin-top: ${toRem(-0.5 * 44 - 47)};
+          transform: scale(0.5);
           transition:
             100ms opacity linear 1100ms,
-            450ms width ${easing.softBack} 1100ms,
-            450ms height ${easing.softBack} 1100ms,
+            450ms margin-top ${easing.softBack} 1100ms,
             450ms transform ${easing.softBack} 1100ms;
         }
       `}
